35664e7ab343bc7e1dfb52dfe574db6b9e803540
Ilan Chelly <ilanchelly@gmail.com>
2026-06-12 15:01:09 +0300

    final

--- main.c
+++ main.c
@@ -15,19 +15,32 @@ int main()
         return 1;
     }
 
-    TransportManagerAdd(tm, 2, "Israel Railways", TRANSPORT_TRAIN, 5000.0, 4200.0);
+    TransportManager tm2 = TransportManagerCreate();
+    if (tm2 == NULL)
+    {
+        fprintf(stderr, "Failed to create TransportManager\n");
+        return 1;
+    }
+    TransportManager merge = TransportManagerCreate();
+    if (merge == NULL)
+    {
+        fprintf(stderr, "Failed to create TransportManager\n");
+        return 1;
+    }
+
+    TransportManagerAdd(tm, 2, "Israel Railways", TRANSPORT_TRAIN, 500.0, 4200.0);
     TransportManagerAdd(tm, 7, "Egged", TRANSPORT_BUS, 3000.0, 3500.0);
     TransportManagerAdd(tm, 1, "Zim", TRANSPORT_SHIPPING, 9000.0, 6000.0);
-    TransportManagerAdd(tm, 5, "El Al", TRANSPORT_FLIGHT, 8000.0, 8000.0);
     TransportManagerAdd(tm, 3, "Gett", TRANSPORT_TAXI, 1200.0, 900.0);
 
-    TransportManagerReportTransportCompanies(tm, TRANSPORT_ALL, stdout);
-    printf("----------------------------------------\n");
-    TransportManagerRemove(tm, 7);
-    TransportManagerRemove(tm, 3);
-    TransportManagerRemove(tm, 3);
-    TransportManagerReportTransportCompanies(tm, TRANSPORT_ALL, stdout);
+    TransportManagerAdd(tm2, 5, "El Al", TRANSPORT_FLIGHT, 8000.0, 8000.0);
+    TransportManagerAdd(tm2, 4, "Gett", TRANSPORT_TAXI, 1200.0, 900.0);
 
+    TransportManagerMerge(tm, tm2, &merge);
+
+    // TransportManagerReportTransportCompanies(merge, TRANSPORT_ALL, stdout);
+    TransportManagerReportCompaniesByNetIncome(merge, stdout);
+    // TransportManagerReportUnprofitableCompanies(merge, stdout);
     TransportManagerDestroy(tm);
     return 0;
 }
--- transport_manager.c
+++ transport_manager.c
@@ -20,6 +20,33 @@ struct TransportManager_s
     Set companies;
 };
 
+static int matchCompanyByType(SetElement elem, KeyForSetElement key)
+{
+    TransportCompany *company = (TransportCompany *)elem;
+    TransportType type = *(TransportType *)key;
+
+    return company->type == type || type == TRANSPORT_ALL;
+}
+
+static int matchCompanyById(SetElement elem, KeyForSetElement key)
+{
+    TransportCompany *company = (TransportCompany *)elem;
+    int id = *(int *)key;
+
+    return company->id == id;
+}
+
+static int matchCompanyByLost(SetElement elem, KeyForSetElement key)
+{
+    TransportCompany *company = (TransportCompany *)elem;
+
+    if (company->expenses >= company->revenue)
+    {
+        return 1;
+    }
+    return 0;
+}
+
 static int compareCompaniesById(SetElement elem1, SetElement elem2)
 {
     TransportCompany *company1 = elem1;
@@ -31,6 +58,26 @@ static int compareCompaniesById(SetElement elem1, SetElement elem2)
     }
 
     if (company1->id > company2->id)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+static int compareCompaniesByProfit(SetElement elem1, SetElement elem2)
+{
+    TransportCompany *company1 = elem1;
+    TransportCompany *company2 = elem2;
+
+    int profit1 = company1->revenue - company1->expenses;
+    int profit2 = company2->revenue - company2->expenses;
+
+    if ((profit1) > (profit2))
+    {
+        return -1;
+    }
+
+    if ((profit1) < (profit2))
     {
         return 1;
     }
@@ -219,7 +266,7 @@ TransportResult TransportManagerRemove(TransportManager tm, int id)
     TransportCompany company_to_remove;
     company_to_remove.id = id;
 
-    TransportResult result = setRemove(tm->companies, &company_to_remove);
+    SetResult result = setRemove(tm->companies, &company_to_remove);
 
     if (result == SET_ELEMENT_DOES_NOT_EXIST)
     {
@@ -234,21 +281,168 @@ TransportResult TransportManagerRemove(TransportManager tm, int id)
     return TRANSPORT_SUCCESS;
 }
 
-static int matchCompanyByType(SetElement elem, KeyForSetElement key)
+TransportResult TransportManagerUpdateRevenue(TransportManager tm, int id, double revenue)
 {
-    TransportCompany *company = elem;
-    TransportType type = *(TransportType *)key;
 
-    return (company->type & type) != 0;
+    if (id <= 0)
+    {
+        return TRANSPORT_INVALID_ID;
+    }
+
+    if (revenue < 0)
+    {
+        return TRANSPORT_INVALID_REVENUE;
+    }
+
+    SetElement found_element = NULL;
+
+    SetResult result = setFind(tm->companies, &found_element, &id, matchCompanyById);
+
+    if (result == SET_ELEMENT_DOES_NOT_EXIST)
+    {
+        return TRANSPORT_DOESNT_EXIST;
+    }
+
+    TransportCompany *company = (TransportCompany *)found_element;
+
+    company->revenue = revenue;
+
+    return TRANSPORT_SUCCESS;
+}
+
+TransportResult TransportManagerUpdateExpenses(TransportManager tm, int id,
+                                               double expenses)
+{
+    if (id <= 0)
+    {
+        return TRANSPORT_INVALID_ID;
+    }
+
+    if (expenses < 0)
+    {
+        return TRANSPORT_INVALID_EXPENSES;
+    }
+
+    SetElement element_to_found = NULL;
+
+    SetResult result = setFind(tm->companies, &element_to_found, &id, matchCompanyById);
+
+    if (result == SET_ELEMENT_DOES_NOT_EXIST)
+    {
+        return TRANSPORT_DOESNT_EXIST;
+    }
+
+    if (result == SET_BAD_ARGUMENTS)
+    {
+        return TRANSPORT_NULL_ARGUMENT;
+    }
+
+    TransportCompany *company = (TransportCompany *)element_to_found;
+    company->expenses = expenses;
+
+    return TRANSPORT_SUCCESS;
+}
+
+TransportResult TransportManagerMerge(TransportManager tm1, TransportManager tm2,
+                                      TransportManager *merged)
+{
+
+    Set intersection = NULL;
+    setIntersection(tm1->companies, tm2->companies, &intersection);
+
+    int n = setGetSize(intersection);
+    setDestroy(intersection);
+    if (n != 0)
+    {
+        return TRANSPORT_ALREADY_EXISTS;
+    }
+
+    TransportManager temp_tm = TransportManagerCreate();
+    if (temp_tm == NULL)
+    {
+        return TRANSPORT_OUT_OF_MEMORY;
+    }
+
+    setDestroy(temp_tm->companies);
+
+    SetResult result = setUnion(tm1->companies, tm2->companies, &temp_tm->companies);
+
+    if (result == SET_OUT_OF_MEMORY)
+    {
+        TransportManagerDestroy(temp_tm);
+        return TRANSPORT_OUT_OF_MEMORY;
+    }
+    if (result == SET_BAD_ARGUMENTS)
+    {
+        TransportManagerDestroy(temp_tm);
+        return TRANSPORT_NULL_ARGUMENT;
+    }
+
+    *merged = temp_tm;
+
+    return TRANSPORT_SUCCESS;
 }
 
 TransportResult TransportManagerReportTransportCompanies(TransportManager tm, TransportType type, FILE *outChannel)
 {
     Set filtered = NULL;
 
-    setFilter(tm->companies, &filtered, &type, matchCompanyByType);
-    setPrint(filtered, outChannel, setGetSize(filtered));
+    SetResult result;
+
+    result = setFilter(tm->companies, &filtered, &type, matchCompanyByType);
+    if (setGetSize(filtered) == 0)
+    {
+        return TRANSPORT_EMPTY;
+    }
+
+    if (result == SET_BAD_ARGUMENTS)
+    {
+        return TRANSPORT_NULL_ARGUMENT;
+    }
+
+    result = setPrintSorted(filtered, outChannel, setGetSize(filtered), compareCompaniesById);
     setDestroy(filtered);
+
+    return TRANSPORT_SUCCESS;
+}
+
+TransportResult TransportManagerReportUnprofitableCompanies(TransportManager tm, FILE *outChannel)
+{
+
+    Set filtered = NULL;
+
+    SetResult result;
+
+    result = setFilter(tm->companies, &filtered, NULL, matchCompanyByLost);
+    if (setGetSize(filtered) == 0)
+    {
+        return TRANSPORT_EMPTY;
+    }
+
+    if (result == SET_BAD_ARGUMENTS)
+    {
+        return TRANSPORT_NULL_ARGUMENT;
+    }
+
+    result = setPrintSorted(filtered, outChannel, setGetSize(filtered), compareCompaniesById);
+    if (result == SET_BAD_ARGUMENTS)
+    {
+        return TRANSPORT_NULL_ARGUMENT;
+    }
+    setDestroy(filtered);
+
+    return TRANSPORT_SUCCESS;
+}
+
+TransportResult TransportManagerReportCompaniesByNetIncome(TransportManager tm, FILE *outChannel)
+{
+
+    if (setGetSize(tm->companies) == 0)
+    {
+        return TRANSPORT_EMPTY;
+    }
+
+    setPrintSorted(tm->companies, stdout, setGetSize(tm->companies), compareCompaniesByProfit);
 
     return TRANSPORT_SUCCESS;
 }
